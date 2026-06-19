e54415f6d2b3dec9ac9e9b428f7e77ab9da63a34
dalaiboada <dalaiboadajob@gmail.com>
2026-06-19 17:13:05 -0400

    feat(MainWin): Crear elementos de la pantalla de bienvenida

--- Archivos del Proyecto/instr.py
+++ Archivos del Proyecto/instr.py
@@ -1,1 +1,18 @@
-# write a code for saving text instructions for the app
+from PyQt5.QtCore import QTime
+
+# Tamaño de ventana
+win_width, win_height = 1000, 600
+
+# Pantalla 1
+txt_hello = '¡Bienvenido al Programa de detección de estado de salud!'
+txt_next = 'Iniciar'
+txt_instruction = ('Esta aplicación le permite usar la prueba de Ruffier para realizar un diagnóstico inicial de su salud.\n'
+                   'La prueba de Ruffier es un conjunto de ejercicios físicos diseñado para evaluar su rendimiento cardíaco durante el esfuerzo físico.\n'
+                   'El sujeto se tumba en posición durante 5 minutos y se toma la frecuencia del pulso durante 15 segundos;\n'
+                   'luego, dentro de 45 segundos, el sujeto realiza 30 sentadillas.\n'
+                   'Cuando el ejercicio termina, el sujeto se tumba y se toman sus pulsaciones de nuevo durante 15 segundos\n'
+                   'y luego durante los últimos 15 segundos del primer minuto del periodo de recuperación.\n'
+                   '¡Importante! Si no se siente bien durante la prueba (mareos,\n'
+                   'tinnitus, falta de respiración, etc.), detenga la prueba y consulte con un médico.' )
+
+txt_title = 'Salud'
--- Archivos del Proyecto/my_app.py
+++ Archivos del Proyecto/my_app.py
@@ -1,1 +1,46 @@
-# write the code for main app and first screen
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+
+from PyQt5.QtWidgets import (
+    QApplication, QWidget,
+    QHBoxLayout, QVBoxLayout, QGridLayout,
+    QGroupBox, QRadioButton,
+    QPushButton, QLabel, QListWidget, QLineEdit
+)
+
+from instr import *
+
+class MainWin(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.set_appear()
+        self.initUI()
+
+        self.show()
+
+    
+    def initUI(self):
+        self.btn_next = QPushButton(txt_next)
+        self.hello_text = QLabel(txt_hello)
+        self.instruction = QLabel(txt_instruction)
+
+        # Contenedor Vertical
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout.addWidget(self.btn_next, alignment = Qt.AlignCenter)
+
+        self.setLayout(self.layout)
+
+    def set_appear(self):
+        self.setWindowTitle(txt_title)
+        self.resize(win_width,win_height)
+
+
+
+def main():
+    app = QApplication([])
+    mw = MainWin()
+    app.exec_()
+
+main()
